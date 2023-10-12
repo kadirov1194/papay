1b16fea6f5bccd6b11d38d2954c2d2e34bff883a
kadirov1194 <shoxrux.qodirov@gmail.com>
2023-10-13 01:06:52 +0900

    Loyihani express qismini shakllantirib olish

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+  "mongodb+srv://wayfay:X2AFCDU4bnwfrdwC@cluster0.lubzyce.mongodb.net/Reja";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("Error on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 7001;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
