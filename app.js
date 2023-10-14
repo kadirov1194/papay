0ceb2fc4e993d3d1b67bc39be69f8fa1cfe07a8a
kadirov1194 <shoxrux.qodirov@gmail.com>
2023-10-14 18:58:03 +0900

    Fix: mongoose connection va member schema model qurish

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
-const router = require("./router");
+const router = require("./router.js");
 
 // MongoDB ni chaqirish
-const db = require("./server").db();
-const mongodb = require("mongodb");
+// const db = require("./server").db();
+// const mongodb = require("mongodb");
 
 // 1: Kirish code
 app.use(express.static("public"));
