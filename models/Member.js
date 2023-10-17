d0c9b019116f9a70a0c91ee51ce6e7c9cb79838b
kadirov1194 <shoxrux.qodirov@gmail.com>
2023-10-17 18:51:31 +0900

    feat: bcrypt package asosida passwordni hash qilish

--- models/Member.js
+++ models/Member.js
@@ -1,6 +1,7 @@
 const MemberModel = require("../schema/member_model"); // schema modelni chaqirib olish
 const Definer = require("../lib/mistake");
 const assert = require("assert");
+const bcrypt = require("bcryptjs");
 
 class Member {
   constructor() {
@@ -9,6 +10,9 @@ class Member {
   //---------------SIGNUP------------
   async signupData(input) {
     try {
+      const salt = await bcrypt.genSalt();
+      input.mb_password = await bcrypt.hash(input.mb_password, salt);
+
       const new_member = new this.memberModel(input);
       let result;
 
@@ -32,10 +36,13 @@ class Member {
         .findOne({ mb_nick: input.mb_nick }, { mb_nick: 1, mb_password: 1 })
         .exec();
 
-      assert(member, Definer.auth_err3);
+      assert.ok(member, Definer.auth_err3);
 
-      const isMatch = input.mb_password === member.mb_password;
-      assert(isMatch, Definer.auth_err4);
+      const isMatch = await bcrypt.compare(
+        input.mb_password,
+        member.mb_password
+      );
+      assert.ok(isMatch, Definer.auth_err4);
 
       return await this.memberModel.findOne({ mb_nick: input.mb_nick }).exec();
     } catch (err) {
