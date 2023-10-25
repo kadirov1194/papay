7669949ddde69611b3a3cfb674846b6cda158b8d
kadirov1194 <shoxrux.qodirov@gmail.com>
2023-10-25 21:18:45 +0900

    fix: moditfy image uploader utils

--- utils/upload-multer.js
+++ utils/upload-multer.js
@@ -2,15 +2,37 @@ const path = require("path"); //core package
 const multer = require("multer");
 const uuid = require("uuid");
 
-const product_storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/products"); // cb - callback
-  },
-  filename: function (req, file, cb) {
-    console.log(file);
-    const extension = path.parse(file.originalname).ext;
-    const random_name = uuid.v4() + extension;
-    cb(null, random_name);
-  },
-});
-module.exports.uploadProductImage = multer({ storage: product_storage });
+/** MULTER IMAGE UPLOADER *  */
+
+function getTargetImageStorage(address) {
+  return multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, `./uploads/${address}`);
+    },
+    filename: function (req, file, cb) {
+      console.log(file);
+      const extension = path.parse(file.originalname).ext;
+      const random_name = uuid.v4() + extension;
+      cb(null, random_name);
+    },
+  });
+}
+const makeUploader = (address) => {
+  const storage = getTargetImageStorage(address);
+  return multer({ storage: storage });
+};
+module.exports = makeUploader;
+
+// SINGLE IMAGE UPLOAD
+// const product_storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads/products"); // cb - callback
+//   },
+//   filename: function (req, file, cb) {
+//     console.log(file);
+//     const extension = path.parse(file.originalname).ext;
+//     const random_name = uuid.v4() + extension;
+//     cb(null, random_name);
+//   },
+// });
+// module.exports.uploadProductImage = multer({ storage: product_storage });
